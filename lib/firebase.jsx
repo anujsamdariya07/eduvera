36fad38de6a6c2b8b4094beeb13c34a9a4fe6189
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-11-05 20:28:01 +0530

    revoking open secrets

--- lib/firebase.jsx
+++ lib/firebase.jsx
@@ -17,20 +17,20 @@ const {
 } = process.env
 
 const firebaseConfig = {
-  // apiKey: NEXT_PUBLIC_API_KEY,
-  // authDomain: NEXT_PUBLIC_AUTH_DOMAIN,
-  // projectId: NEXT_PUBLIC_PROJECT_ID,
-  // storageBucket: NEXT_PUBLIC_STORAGE_BUCKET,
-  // messagingSenderId: NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  // appId: NEXT_PUBLIC_APP_ID,
-  // measurementId: NEXT_PUBLIC_MEASUREMENT_ID
-  apiKey: "AIzaSyCaXfKeQtcYLZdCuyN3OgkSPgDVkjlIeFc",
-  authDomain: "eduvera-acbc6.firebaseapp.com",
-  projectId: "eduvera-acbc6",
-  storageBucket: "eduvera-acbc6.firebasestorage.app",
-  messagingSenderId: "658665988434",
-  appId: "1:658665988434:web:fc01abe376bfb5d1011a8b",
-  measurementId: "G-XMS18SM24G"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  // apiKey: "AIzaSyCaXfKeQtcYLZdCuyN3OgkSPgDVkjlIeFc",
+  // authDomain: "eduvera-acbc6.firebaseapp.com",
+  // projectId: "eduvera-acbc6",
+  // storageBucket: "eduvera-acbc6.firebasestorage.app",
+  // messagingSenderId: "658665988434",
+  // appId: "1:658665988434:web:fc01abe376bfb5d1011a8b",
+  // measurementId: "G-XMS18SM24G"
 };
 
 let app;
